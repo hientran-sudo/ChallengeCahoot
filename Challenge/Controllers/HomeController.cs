a1caa18e1a69aa5cd5a1fb3c746a13b2f9554a4e
hientran-sudo <hien.tran@student.fairfield.edu>
2022-01-16 20:32:38 -0500

    Add project files.

---
+++ Challenge/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Challenge.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+namespace Challenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+
+
+        public HomeController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+        public IActionResult Index()
+        {
+            string connectionstring = configuration.GetConnectionString("Default");
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
